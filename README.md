e4ce8aeac4a6156c1b42e8d6a0f9bc817ab1e683
anandhums997 <anandhums635@gmail.com>
2024-06-17 17:07:12 +0530

    added dependency injection using typedi and updated code for edit user detail

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # express-rest-api-boilerplate
  An Express.js REST API boilerplate
+
+
+
--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,4 +1,4 @@
 import { myDataSource } from "../app-data-source";
 import { User } from "../entity/user.entity";
 
-export const UserRepository = myDataSource.getRepository(User)
+export const UserRepository = myDataSource.getRepository(User);
